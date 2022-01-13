5260c9b596dea51b2ca4150ac2af8623a01c003d
sadique <1234sadique1234@gmail.com>
2022-01-13 13:09:24 +0530

    update all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AssignmentofGrid';
+  showImage:boolean = false;
+  show:any;
+  arrayOfImage:any[] = [
+   {url:'assets/sportitems.jpg'},{url:'assets/xcvx.jpg'},{url:'assets/casualshoses.jpg'}, {url:'assets/back.jpg'},{url:'assets/sunglass.jpg'},{url:'assets/,kj.jpg'}];
+  abc(getImage: any){
+    this.showImage=true;
+    this.show=getImage;
+    console.log(getImage);
+    // addItem(index) {
+      // var currentElement = this.arrayOfImage[getImage];
+      // this.arrayOfImage.splice(getImage, 0, currentElement);
+    // }
+  }
+
 }
